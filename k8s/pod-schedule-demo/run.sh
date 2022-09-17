4a50ff38fcb085a7daf9783d62d74f2a7c712882
leo <qianxunlan@qq.com>
2022-09-17 20:35:19 +0800

    update k8s pods schedule with node shutdown scene

--- k8s/pod-schedule-demo/run.sh
+++ k8s/pod-schedule-demo/run.sh
@@ -2,8 +2,19 @@
 
 kubectl apply -f deployment.yaml
 kubectl get pods -o wide
+# 删除一个，又自动添加一个，保持replicas数量
 kubectl delete pods webserver-xxxxxxxx
 kubectl get pods -o wide
+# https://kubernetes.io/zh-cn/docs/concepts/architecture/nodes/#condition
+# 5分钟超时后自动添加一个新的，该机器上此Pod为Terminating
+minikube node stop minikube-m03
+# 添加以下可缩短超时时间
+#   tolerations:
+#     - key: "node.kubernetes.io/unreachable"
+#       operator: "Exists"
+#       effect: "NoExecute"
+#       tolerationSeconds: 10
+    
 
 kubectl apply -f deployment-nodeSelector.yaml
 kubectl get pods -o wide
